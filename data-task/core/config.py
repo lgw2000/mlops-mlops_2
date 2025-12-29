d0fa6a5b12561cc7cfb01398473ff1db64986a64
KJH-0596 <jhkim000601@naver.com>
2025-12-29 01:23:54 +0000

    feat: add TMDB key setting

--- data-task/core/config.py
+++ data-task/core/config.py
@@ -5,7 +5,12 @@ from dotenv import load_dotenv
 # .env파일 로드
 load_dotenv()
 
+# AWS Setting
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 region_name=os.getenv('AWS_REGION', 'ap-northeast-2')
 bucket_name=os.getenv('S3_BUCKET')
+
+# TMDB Settings
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+TMDB_BASE_URL = "https://api.themoviedb.org/3"
