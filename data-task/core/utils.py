0c2f27bf72b252873e3b194adf0bbfd068205514
NullXeronier <next.forr@gmail.com>
2025-12-29 16:53:11 +0900

    feat: CI/CD broken solve

--- data-task/core/utils.py
+++ data-task/core/utils.py
@@ -6,7 +6,7 @@ def save_to_local(base_dir: str, df: pd.DataFrame) -> tuple[bool, str]:
     try:
         if not os.path.exists(base_dir):
             os.makedirs(base_dir)
-            print(f"디렉터리 생성 완료: {output_dir}")
+            print(f"디렉터리 생성 완료: {base_dir}")
         
         timestamp = datetime.now().strftime("%Y%m%d"+"_"+"%H%M%S")
         filename = f"{timestamp}.csv"
